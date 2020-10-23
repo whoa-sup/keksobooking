9030b2369c83f5fe0ddf87543433474e5c80fce6
Semen Panfilov <whoasup@gmail.com>
2020-10-23 21:13:27 +0300

    Сделал сборку bundle.js с помощью webpack

--- js/data.js
+++ js/data.js
@@ -1,104 +1,101 @@
 'use strict';
 
-(() => {
+/**
+ * Генерирует заданное количество моков объявлений
+ * @param {number} count - число необходимых моков
+ * @return {Array} массив объявлений
+ */
+const generateMockAds = (count) => {
+  const map = document.querySelector(`.map`);
+  const TITLES = [
+    `Мило, уютно`,
+    `Мило,но дорого`,
+    `Неуютно, но снимите`,
+    `Просто объявление`,
+    `Ужасно дорого`,
+    `Не стоит снимать у меня квартиру`,
+  ];
+  const DESCRIPTIONS = [
+    `О господи, какое это замечательно жилье`,
+    `Квартира то замечательная, но соседи просто ад`,
+    `В окне на против вы сможете увидеть - ничего не сможете увидеть`,
+    `Терпеть не могу придумывать описания для всяких вещей`,
+    `ААААААААААА, снимите, срочно нужны деньги`,
+  ];
+  const TYPES = [
+    `palace`,
+    `flat`,
+    `house`,
+    `bungalow`,
+  ];
+  const FEATURES = [
+    `wifi`,
+    `dishwasher`,
+    `parking`,
+    `washer`,
+    `elevator`,
+    `conditioner`,
+  ];
+  const PHOTOS = [
+    `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+    `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+    `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
+  ];
+  const ROOMS_NUMBER = [1, 2, 3, 100];
+  const GUESTS_NUMBER = [1, 2, 3, 0];
+  const CHECKINS = [`12:00`, `13:00`, `14:00`];
+  const CHECKOUTS = [`12:00`, `13:00`, `14:00`];
+  const MIN_PRICE = 0;
+  const MAX_PRICE = 10000;
+  const MIN_X = 0;
+  const MAX_X = map.offsetWidth;
+  const MIN_Y = 130;
+  const MAX_Y = 630;
+
   /**
-   * Генерирует заданное количество моков объявлений
-   * @param {number} count - число необходимых моков
-   * @return {Array} массив объявлений
+   * Генерирует массив номеров аватаров
+   * @param {number} size количество номеров аватаров
+   * @return {Array} масиив строк типа "0" + number, заданной длины
    */
-  const generateMockAds = (count) => {
-    const map = document.querySelector(`.map`);
-    const TITLES = [
-      `Мило, уютно`,
-      `Мило,но дорого`,
-      `Неуютно, но снимите`,
-      `Просто объявление`,
-      `Ужасно дорого`,
-      `Не стоит снимать у меня квартиру`,
-    ];
-    const DESCRIPTIONS = [
-      `О господи, какое это замечательно жилье`,
-      `Квартира то замечательная, но соседи просто ад`,
-      `В окне на против вы сможете увидеть - ничего не сможете увидеть`,
-      `Терпеть не могу придумывать описания для всяких вещей`,
-      `ААААААААААА, снимите, срочно нужны деньги`,
-    ];
-    const TYPES = [
-      `palace`,
-      `flat`,
-      `house`,
-      `bungalow`,
-    ];
-    const FEATURES = [
-      `wifi`,
-      `dishwasher`,
-      `parking`,
-      `washer`,
-      `elevator`,
-      `conditioner`,
-    ];
-    const PHOTOS = [
-      `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
-      `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
-      `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
-    ];
-    const ROOMS_NUMBER = [1, 2, 3, 100];
-    const GUESTS_NUMBER = [1, 2, 3, 0];
-    const CHECKINS = [`12:00`, `13:00`, `14:00`];
-    const CHECKOUTS = [`12:00`, `13:00`, `14:00`];
-    const MIN_PRICE = 0;
-    const MAX_PRICE = 10000;
-    const MIN_X = 0;
-    const MAX_X = map.offsetWidth;
-    const MIN_Y = 130;
-    const MAX_Y = 630;
-
-    /**
-     * Генерирует массив номеров аватаров
-     * @param {number} size количество номеров аватаров
-     * @return {Array} масиив строк типа "0" + number, заданной длины
-     */
-    const generateAvatarUrls = (size) => {
-      const array = [];
-      for (let i = 1; i <= size; i++) {
-        array.push(`0` + i);
-      }
-      return array;
-    };
-
-    const avatarUrls = window.util.shuffleArray(generateAvatarUrls(count));
-
-    const ads = [];
-    for (let i = 0; i < count; i++) {
-      const ad = {
-        author: {
-          avatar: `img/avatars/user${avatarUrls[i]}.png`,
-        },
-        offer: {
-          title: window.util.getRandomArrayElement(TITLES),
-          price: window.util.getRandomNumberFromRange(MIN_PRICE, MAX_PRICE),
-          type: window.util.getRandomArrayElement(TYPES),
-          rooms: window.util.getRandomArrayElement(ROOMS_NUMBER),
-          guests: window.util.getRandomArrayElement(GUESTS_NUMBER),
-          checkin: window.util.getRandomArrayElement(CHECKINS),
-          checkout: window.util.getRandomArrayElement(CHECKOUTS),
-          features: window.util.copyArrayRandomElements(FEATURES),
-          description: window.util.getRandomArrayElement(DESCRIPTIONS),
-          photos: window.util.copyArrayRandomElements(PHOTOS),
-        },
-        location: {
-          x: window.util.getRandomNumberFromRange(MIN_X, MAX_X),
-          y: window.util.getRandomNumberFromRange(MIN_Y, MAX_Y),
-        },
-      };
-      ad.offer.address = `${ad.location.x}, ${ad.location.y}`;
-      ads.push(ad);
+  const generateAvatarUrls = (size) => {
+    const array = [];
+    for (let i = 1; i <= size; i++) {
+      array.push(`0` + i);
     }
-    return ads;
+    return array;
   };
 
-  window.data = {
-    generateAds: generateMockAds,
-  };
+  const avatarUrls = window.util.shuffleArray(generateAvatarUrls(count));
 
-})();
+  const ads = [];
+  for (let i = 0; i < count; i++) {
+    const ad = {
+      author: {
+        avatar: `img/avatars/user${avatarUrls[i]}.png`,
+      },
+      offer: {
+        title: window.util.getRandomArrayElement(TITLES),
+        price: window.util.getRandomNumberFromRange(MIN_PRICE, MAX_PRICE),
+        type: window.util.getRandomArrayElement(TYPES),
+        rooms: window.util.getRandomArrayElement(ROOMS_NUMBER),
+        guests: window.util.getRandomArrayElement(GUESTS_NUMBER),
+        checkin: window.util.getRandomArrayElement(CHECKINS),
+        checkout: window.util.getRandomArrayElement(CHECKOUTS),
+        features: window.util.copyArrayRandomElements(FEATURES),
+        description: window.util.getRandomArrayElement(DESCRIPTIONS),
+        photos: window.util.copyArrayRandomElements(PHOTOS),
+      },
+      location: {
+        x: window.util.getRandomNumberFromRange(MIN_X, MAX_X),
+        y: window.util.getRandomNumberFromRange(MIN_Y, MAX_Y),
+      },
+    };
+    ad.offer.address = `${ad.location.x}, ${ad.location.y}`;
+    ads.push(ad);
+  }
+  return ads;
+};
+
+window.data = {
+  generateAds: generateMockAds,
+};
--- js/filter.js
+++ js/filter.js
@@ -1,90 +1,88 @@
 'use strict';
 
-(() => {
-  const MAX_ADS_COUNT = 5;
-  const filterTypeSelect = document.querySelector(`#housing-type`);
-  const filterRoomsSelect = document.querySelector(`#housing-rooms`);
-  const filterGuestsSelect = document.querySelector(`#housing-guests`);
-  const filterPriceSelect = document.querySelector(`#housing-price`);
-  const filterFeaturesCollection = document.querySelectorAll(`#housing-features input[type="checkbox"]`);
+const MAX_ADS_COUNT = 5;
+const filterTypeSelect = document.querySelector(`#housing-type`);
+const filterRoomsSelect = document.querySelector(`#housing-rooms`);
+const filterGuestsSelect = document.querySelector(`#housing-guests`);
+const filterPriceSelect = document.querySelector(`#housing-price`);
+const filterFeaturesCollection = document.querySelectorAll(`#housing-features input[type="checkbox"]`);
 
-  const filterRules = {
-    'housing-type': {
-      elem: filterTypeSelect,
-      isMatches: function (data) {
-        return (this.elem.value === `any`) ? true : this.elem.value === data.offer.type;
+const filterRules = {
+  'housing-type': {
+    elem: filterTypeSelect,
+    isMatches: function (data) {
+      return (this.elem.value === `any`) ? true : this.elem.value === data.offer.type;
+    },
+  },
+  'housing-rooms': {
+    elem: filterRoomsSelect,
+    isMatches: function (data) {
+      return (this.elem.value === `any`) ? true : this.elem.value === data.offer.rooms.toString();
+    },
+  },
+  'housing-guests': {
+    elem: filterGuestsSelect,
+    isMatches: function (data) {
+      return (this.elem.value === `any`) ? true : this.elem.value === data.offer.guests.toString();
+    },
+  },
+  'housing-price': {
+    elem: filterPriceSelect,
+    map: {
+      low: {
+        start: 0,
+        end: 10000
+      },
+      middle: {
+        start: 10000,
+        end: 50000
+      },
+      high: {
+        start: 50000,
+        end: Infinity
       },
     },
-    'housing-rooms': {
-      elem: filterRoomsSelect,
-      isMatches: function (data) {
-        return (this.elem.value === `any`) ? true : this.elem.value === data.offer.rooms.toString();
-      },
+    isMatches: function (data) {
+      return (this.elem.value === `any`) ? true : data.offer.price >= this.map[this.elem.value].start && data.offer.price <= this.map[this.elem.value].end;
     },
-    'housing-guests': {
-      elem: filterGuestsSelect,
-      isMatches: function (data) {
-        return (this.elem.value === `any`) ? true : this.elem.value === data.offer.guests.toString();
-      },
-    },
-    'housing-price': {
-      elem: filterPriceSelect,
-      map: {
-        low: {
-          start: 0,
-          end: 10000
-        },
-        middle: {
-          start: 10000,
-          end: 50000
-        },
-        high: {
-          start: 50000,
-          end: Infinity
-        },
-      },
-      isMatches: function (data) {
-        return (this.elem.value === `any`) ? true : data.offer.price >= this.map[this.elem.value].start && data.offer.price <= this.map[this.elem.value].end;
-      },
-    },
-    'housing-features': {
-      checkboxes: filterFeaturesCollection,
-      isMatches: function (data) {
-        let match = true;
-        for (const checkbox of this.checkboxes) {
-          if (match) {
-            match = checkbox.checked === false ? true : data.offer.features.includes(checkbox.value);
-          }
-        }
-        return match;
-      },
-    },
-  };
-
-  /**
-   * Возвращает отфильтрованный массив с количеством элементов не более MAX_ADS_COUNT
-   * @param {Array} data - массив который необходимо отфильтровать
-   * @return {Array} - Отфильтрованный массив
-   */
-  const filterData = (data) => {
-    const filteredData = [];
-
-    for (let i = 0; i < data.length; i++) {
-      if (filteredData.length < MAX_ADS_COUNT) {
-        let flag = true;
-        for (const rule in filterRules) {
-          if (flag) {
-            flag = filterRules[rule].isMatches(data[i]);
-          }
-        }
-        if (flag) {
-          filteredData.push(data[i]);
+  },
+  'housing-features': {
+    checkboxes: filterFeaturesCollection,
+    isMatches: function (data) {
+      let match = true;
+      for (const checkbox of this.checkboxes) {
+        if (match) {
+          match = checkbox.checked === false ? true : data.offer.features.includes(checkbox.value);
         }
       }
+      return match;
+    },
+  },
+};
+
+/**
+ * Возвращает отфильтрованный массив с количеством элементов не более MAX_ADS_COUNT
+ * @param {Array} data - массив который необходимо отфильтровать
+ * @return {Array} - Отфильтрованный массив
+ */
+const filterData = (data) => {
+  const filteredData = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (filteredData.length < MAX_ADS_COUNT) {
+      let flag = true;
+      for (const rule in filterRules) {
+        if (flag) {
+          flag = filterRules[rule].isMatches(data[i]);
+        }
+      }
+      if (flag) {
+        filteredData.push(data[i]);
+      }
     }
+  }
 
-    return filteredData;
-  };
+  return filteredData;
+};
 
-  window.filter = filterData;
-})();
+window.filter = filterData;
